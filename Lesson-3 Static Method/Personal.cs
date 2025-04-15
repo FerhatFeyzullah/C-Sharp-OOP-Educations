4b4701b0b0023bf19de0793a9ccc0339b4cf334a
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-15 16:44:25 +0300

    static method usage

---
+++ Lesson-3 Static Method/Personal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Static_Method
+{
+    public class Personal
+    {
+
+        public static void Hesapla(int sayi1,int sayi2)
+        {
+            Console.WriteLine("Verilmis Olan Iki Sayinin Toplami: "+(sayi1+sayi2));
+        }
+    }
+}
---
+++ Lesson-3 Static Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Static_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+            Metot static olarak belirlendiginde bulundugu class tan nesne olusturmaya gerek kalmadan
+            direkt metot cagrisi yapilabilir
+            */
+            Personal.Hesapla(54, 61);
+
+        }
+    }
+}
