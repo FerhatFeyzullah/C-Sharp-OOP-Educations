ae1c74b93a6620d1165864c0df546fdf3602b812
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-15 19:55:00 +0300

    static class usage

---
+++ Static Class/Musteri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class
+{
+    public static class Musteri
+    {
+        //Static Classlardan Nesne Olusturulmaz.
+        //Static classlarin icinde static olmayan bir sey tanimlanmaz
+
+
+
+        
+        public static int id;
+        public static string name;
+
+        static Musteri()
+        {
+            id = 54;
+            name = "Ferhat";
+        }
+
+        public static void huhu()
+        {
+            Console.WriteLine("The Name Is: "+name+"\nHis ID: "+id);
+        }
+
+
+    }
+}
---
+++ Static Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Static classlardan nesne uretilmez.
+            // Musteri M1 = new Musteri();
+
+
+            //static olarak kullanilabilir tabiki
+            Musteri.huhu();
+        }
+    }
+}
