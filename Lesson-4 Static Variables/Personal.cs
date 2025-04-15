b8df856ff12614aa671118b622090e8e87e649e0
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-15 19:02:27 +0300

    static varibles usage

---
+++ Lesson-4 Static Variables/Personal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Static_Variables
+{
+    public class Personal
+    {
+
+        public int Id = 54;
+        public string firstName = "Ferhat";
+
+
+        public static string lastName = "Feyzullah";
+    }
+}
---
+++ Lesson-4 Static Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Static_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personal p1 = new Personal();
+            Console.WriteLine("Personal ID: "+p1.Id+ "\nPersonal Name: "+p1.firstName+"\nPersonal Surname: "+Personal.lastName);
+            //son degiskeni static olarak belirledigim icin personal classindan nesne olusturmadan da 
+            //kendisine ulasabildim.
+
+
+        }
+    }
+}
