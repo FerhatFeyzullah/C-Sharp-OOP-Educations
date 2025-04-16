0b2daf67d70c150bc324783e31057ce5d35c06a5
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-16 09:23:53 +0300

    array list usage

---
+++ Lesson-7 Array List/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_Array_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList list = new ArrayList();
+
+            list.Add(54);
+            list.Add("Ferhat");
+            list.Add("Feyzullah");
+
+            //list.Reverse();
+            //list.Clear();
+            //list.Contains(54);
+            //int a = list.Count;
+            //list.Insert(0, "HUHU");
+            //int b = list.IndexOf(54);
+            //list.Sort();
+            //list.RemoveAt(0);
+            //list.Remove(54);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
